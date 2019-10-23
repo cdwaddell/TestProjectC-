422ee91e895e381de8d9c8a479fca9b07806c9fc
Daniel Waddell <Dwaddell@hmbnet.com>
2019-10-23 19:26:40 -0400

    initial commit

---
+++ TestProject1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestProject1
+{
+    class WhateverYouWant
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
